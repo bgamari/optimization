600abd83bf0cc1024c6276e5200c1e5ae700e910
Ben Gamari <bgamari.foss@gmail.com>
2013-03-15 13:17:52 -0400

    SteepestDescent: Add comments

--- src/Optimization/LineSearch/SteepestDescent.hs
+++ src/Optimization/LineSearch/SteepestDescent.hs
@@ -7,6 +7,12 @@ import Optimization.LineSearch
 import Linear
 
 -- | Steepest descent method
+--
+-- @steepestDescent search f df x0@ optimizes a function @f@ with gradient @df@
+-- with step size schedule @search@ starting from initial point @x0@
+--
+-- The steepest descent method chooses the negative gradient of the function
+-- as its step direction.
 {-# INLINEABLE steepestDescent #-}
 steepestDescent :: (Num a, Ord a, Additive f, Metric f)
                 => LineSearch f a -> (f a -> a) -> (f a -> f a) -> f a -> [f a]
