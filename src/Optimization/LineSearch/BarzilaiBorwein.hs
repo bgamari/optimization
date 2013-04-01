bd525badc3192ab66e6a65a88e3f69fd4714965c
Ben Gamari <bgamari.foss@gmail.com>
2013-04-01 16:50:58 -0400

    BarzilaiBorwein: Clean up warnings

--- src/Optimization/LineSearch/BarzilaiBorwein.hs
+++ src/Optimization/LineSearch/BarzilaiBorwein.hs
@@ -7,11 +7,11 @@ import Linear
 -- | Barzilai-Borwein 1988 is a non-monotonic optimization method
 barzilaiBorwein :: (Additive f, Metric f, Functor f, Fractional a, Epsilon a)
                 => (f a -> f a) -> f a -> f a -> [f a]
-barzilaiBorwein df x0 x1 = go (x0, x1)
-  where go (x0,x1) = let s = x1 ^-^ x0
-                         z = df x1 ^-^ df x0
-                         alpha = (s `dot` z) / (z `dot` z)
-                         x2 = x1 ^-^ alpha *^ df x1
-                     in if nearZero (z `dot` z)
-                          then [x2]
-                          else x2 : go (x1, x2)
+barzilaiBorwein df = go
+  where go x0 x1 = let s = x1 ^-^ x0
+                       z = df x1 ^-^ df x0
+                       alpha = (s `dot` z) / (z `dot` z)
+                       x2 = x1 ^-^ alpha *^ df x1
+                   in if nearZero (z `dot` z)
+                        then [x2]
+                        else x2 : go x1 x2
