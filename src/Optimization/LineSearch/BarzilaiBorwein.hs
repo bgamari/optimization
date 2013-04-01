043fd4820fe628390e2d8bae486121deff896095
Ben Gamari <bgamari.foss@gmail.com>
2013-04-01 15:34:33 -0400

    BarzilaiBorwein: Handle convergence gracefully
    z `dot` z converges to zero as we approach the optimum, leading to
    diverging alphas. Terminate the iterations when this happens

--- src/Optimization/LineSearch/BarzilaiBorwein.hs
+++ src/Optimization/LineSearch/BarzilaiBorwein.hs
@@ -5,11 +5,13 @@ module Optimization.LineSearch.BarzilaiBorwein
 import Linear
 
 -- | Barzilai-Borwein 1988 is a non-monotonic optimization method
-barzilaiBorwein :: (Additive f, Metric f, Functor f, Fractional a)
+barzilaiBorwein :: (Additive f, Metric f, Functor f, Fractional a, Epsilon a)
                 => (f a -> f a) -> f a -> f a -> [f a]
 barzilaiBorwein df x0 x1 = go (x0, x1)
   where go (x0,x1) = let s = x1 ^-^ x0
                          z = df x1 ^-^ df x0
                          alpha = (s `dot` z) / (z `dot` z)
                          x2 = x1 ^-^ alpha *^ df x1
-                     in x2 : go (x1, x2)
+                     in if nearZero (z `dot` z)
+                          then [x2]
+                          else x2 : go (x1, x2)
