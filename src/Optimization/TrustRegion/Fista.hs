e9779b847204684c078f6a6bb9b933ef692faf93
Ben Gamari <bgamari.foss@gmail.com>
2013-04-01 17:02:48 -0400

    Fista: Silence warnings

--- src/Optimization/TrustRegion/Fista.hs
+++ src/Optimization/TrustRegion/Fista.hs
@@ -9,8 +9,8 @@ import Linear
 -- constant stepsize
 {-# INLINEABLE fista #-}
 fista :: (Additive f, Fractional a, Floating a)
-      => a -> (f a -> a) -> (f a -> f a) -> f a -> [f a]
-fista l f df x0 = go x0 x0 1
+      => a -> (f a -> f a) -> f a -> [f a]
+fista l df x0' = go x0' x0' 1
   where go x0 y1 t1 = let x1 = y1 ^-^ df y1 ^/ l
                           t2 = (1 + sqrt (1 + 4 * t1^2)) / 2
                           y2 = x1 ^+^ (t1-1) / t2 *^ (x1 ^-^ x0)
