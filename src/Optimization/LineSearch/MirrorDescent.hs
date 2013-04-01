71202b7e688e848d28e144f86aff902ec8f75255
Ben Gamari <bgamari.foss@gmail.com>
2013-04-01 16:52:54 -0400

    MirrorDescent: Clean up warning

--- src/Optimization/LineSearch/MirrorDescent.hs
+++ src/Optimization/LineSearch/MirrorDescent.hs
@@ -15,7 +15,7 @@ import Linear
 mirrorDescent :: (Num a, Additive f)
               => LineSearch f a -> (f a -> f a) -> (f a -> f a)
               -> (f a -> f a) -> f a -> [f a]
-mirrorDescent search dPsi dPsiStar df y0 = go y0
+mirrorDescent search dPsi dPsiStar df = go
   where go y0 = let x0 = dPsiStar y0
                     t0 = search df (df x0) x0
                     y1 = dPsi x0 ^-^ t0 *^ df x0
