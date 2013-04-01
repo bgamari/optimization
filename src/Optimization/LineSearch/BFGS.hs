b456409f7676e09aca98c4bf76ac194ebe789dae
Ben Gamari <bgamari.foss@gmail.com>
2013-04-01 15:12:48 -0400

    BFGS: Fix Hessian update

--- src/Optimization/LineSearch/BFGS.hs
+++ src/Optimization/LineSearch/BFGS.hs
@@ -4,13 +4,16 @@ module Optimization.LineSearch.BFGS (bfgs) where
 
 import Linear
 import Optimization.LineSearch
+import Control.Applicative
+import Data.Traversable
 import Data.Distributive
 import Data.Foldable
 
 -- | Broyden–Fletcher–Goldfarb–Shanno (BFGS) method
 -- @bfgs search df b0 x0@ where @b0@ is the inverse Hessian (the
 -- identity is often a good initial value).
-bfgs :: (Metric f, Additive f, Distributive f, Foldable f, Num a, Fractional a)
+bfgs :: ( Metric f, Additive f, Distributive f, Foldable f, Traversable f, Applicative f
+        , Fractional a, Epsilon a)
      => LineSearch f a -> (f a -> f a) -> f (f a) -> f a -> [f a]
 bfgs search df = go
     where go b0 x0 = let p1 = negated $ b0 !* df x0
@@ -20,9 +23,11 @@ bfgs search df = go
                          y = df x1 ^-^ df x0
                          -- Sherman-Morrison update of inverse Hessian
                          sy = s `dot` y
-                         u = ((sy + y `dot` (b0 !* y)) *!! outer s s) !!/ (sy^2)
-                         v = (b0 !*! outer y s !+! outer s y !*! b0) !!/ sy
-                         b1 = b0 !+! u !-! v
+                         rho = if nearZero sy then 1000 else 1 / sy
+                         i = kronecker (pure 1)
+                         u = i !-! rho *!! outer y s
+                         v = i !-! rho *!! outer s y
+                         b1 = u !*! b0 !*! v !+! rho *!! outer s s
                      in x1 : go b1 x1
 
 (!!/) :: (Functor f, Functor g, Fractional a) => f (g a) -> a -> f (g a)
