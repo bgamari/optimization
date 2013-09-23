c88de5c5ba24f3fd30f11950df5a603b3b158da8
Ben Gamari <bgamari.foss@gmail.com>
2013-09-23 09:58:41 -0400

    Newton: Add INLINEs

--- src/Optimization/TrustRegion/Newton.hs
+++ src/Optimization/TrustRegion/Newton.hs
@@ -13,11 +13,11 @@ import Data.Foldable (Foldable)
 import Linear
 
 -- | Newton's method
-{-# INLINEABLE newton #-}
 newton :: (Num a, Ord a, Additive f, Metric f, Foldable f)
        => (f a -> f a) -> (f a -> f (f a)) -> f a -> [f a]
 newton df ddfInv x0 = iterate go x0
   where go x = x ^-^ ddfInv x !* df x
+{-# INLINEABLE newton #-}
 
 -- | Inverse by iterative method of Ben-Israel and Cohen
 -- with given starting condition
@@ -26,6 +26,7 @@ bicInv' :: (Functor m, Distributive m, Additive m,
         => m (m a) -> m (m a) -> [m (m a)]
 bicInv' a0 a = iterate go a0
   where go ak = 2 *!! ak !-! ak !*! a !*! ak
+{-# INLINEABLE bicInv' #-}
 
 -- | Inverse by iterative method of Ben-Israel and Cohen
 -- starting from 'alpha A^T'. Alpha should be set such that
@@ -35,3 +36,5 @@ bicInv :: (Functor m, Distributive m, Additive m,
            Applicative m, Apply m, Foldable m, Conjugate a)
        => a -> m (m a) -> [m (m a)]
 bicInv alpha a = bicInv' (alpha *!! adjoint a) a
+{-# INLINEABLE bicInv #-}
+
