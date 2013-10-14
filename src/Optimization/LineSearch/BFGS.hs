849e97527a300f0897831f06d2846d525f5e0fa3
Ben Gamari <bgamari.foss@gmail.com>
2013-10-13 22:32:43 -0400

    BFGS: Documentation

--- src/Optimization/LineSearch/BFGS.hs
+++ src/Optimization/LineSearch/BFGS.hs
@@ -14,7 +14,11 @@ import Data.Foldable
 -- identity is often a good initial value).
 bfgs :: ( Metric f, Additive f, Distributive f, Foldable f, Traversable f, Applicative f
         , Fractional a, Epsilon a)
-     => LineSearch f a -> (f a -> f a) -> f (f a) -> f a -> [f a]
+     => LineSearch f a   -- ^ line search method
+     -> (f a -> f a)     -- ^ gradient of function
+     -> f (f a)          -- ^ inverse Hessian at starting point
+     -> f a              -- ^ starting point
+     -> [f a]            -- ^ iterates
 bfgs search df = go
     where go b0 x0 = let p1 = negated $ b0 !* df x0
                          alpha = search df p1 x0
