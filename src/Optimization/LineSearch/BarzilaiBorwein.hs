048c137f087a862a63e9361823f7e79b0029b097
Ben Gamari <bgamari.foss@gmail.com>
2013-10-13 22:33:43 -0400

    BarzilaiBorwein: Documentation

--- src/Optimization/LineSearch/BarzilaiBorwein.hs
+++ src/Optimization/LineSearch/BarzilaiBorwein.hs
@@ -6,7 +6,10 @@ import Linear
 
 -- | Barzilai-Borwein 1988 is a non-monotonic optimization method
 barzilaiBorwein :: (Additive f, Metric f, Functor f, Fractional a, Epsilon a)
-                => (f a -> f a) -> f a -> f a -> [f a]
+                => (f a -> f a)  -- ^ gradient of function
+                -> f a           -- ^ starting point, @x0@
+                -> f a           -- ^ second starting point, @x1@
+                -> [f a]         -- ^ iterates
 barzilaiBorwein df = go
   where go x0 x1 = let s = x1 ^-^ x0
                        z = df x1 ^-^ df x0
